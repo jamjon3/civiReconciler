6ad6166d6240a61e352b78dba323f13deb69b9dc
James Jones <jamjon3@gmail.com>
2018-05-05 23:53:27 -0400

    Getting the controller closer to wiring

--- app/Http/Controllers/CiviCRM/ContactController.php
+++ app/Http/Controllers/CiviCRM/ContactController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use YMD\CiviCRMconnector\Contact;
+use YMD\CiviCRMconnector\Entity\Contact;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ContactController extends Controller 
-{
-
+{  
+  public function findById(Request $request): array {
+    return Contact::findById($request->get('id'));
+  }
+  public function create(Request $request) {
+    return Contact::save($request->all());
+  }
 }
